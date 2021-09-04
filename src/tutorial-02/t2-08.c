0be0ca7322ccf159cd9ac86a1ab65037f5859916
Arun Raj R <arunraj6@hotmail.com>
2021-09-04 20:35:14 +0530

    update the folder structure

---
+++ src/tutorial-02/t2-08.c
@@ -0,0 +1,10 @@
+// Modify the concentration game to use main menu. 
+// The menu should allow the user to select a level of difficulty and/or quit the game.
+// Each time the user completes a single game,
+// the menu should reappear allowing the user to continue at the same level,
+// at a new level, of simply quit the game
+
+// Easy (remember 3 numbers in 5 seconds)
+// Intermediate (remember 5 numbers in 5 seconds)
+// Difficult (remember 5 numbers in 2 seconds)
+// Quit
---
+++ src/tutorial-03/t3-01.c
@@ -0,0 +1,12 @@
+// Write a program that requests two integer values and then,
+// calls functions (by defining function prototypes and function definitions)
+// to find the following:
+
+// the sum of two numbers,
+// the larger of two numbers,
+// the difference of two numbers,
+// the product of the two numbers,
+// the quotient from the division of the two numbers (with the larger number as the dividend and the smaller number as the divisor)
+// the remainder from the division of the two numbers (with the larger number as the dividend and the smaller number as the divisor)
+// the  average of the two number.
+// The program then prints all these values.
